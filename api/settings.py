b370cca53e208cd15ebb2f8f28eb4ae3e435257c
hhectormanuel <49839790+hhectormanuel@users.noreply.github.com>
2022-11-29 18:43:22 -0600

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -63,7 +63,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://10.0.2.2:8000",
     "http://10.0.2.2:8081",
     "http://127.0.0.1:8000",
-    "https://pwa-8xro.onrender.com".
+    "https://pwa-8xro.onrender.com",
     "https://lavenganzademaria.000webhostapp.com"
 ]
 
